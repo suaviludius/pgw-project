6805c7340f8ba669ac55cd95ff38d15a35de0870
Artem <powerranger1912@gmail.com>
2025-12-25 23:21:07 +0300

    perf(udp-server): добавлено наследование от интерфейсного класса; сокет теперь  unique_ptr и создается в конструкторе, а в start() выполняктся его bind()

--- src/server/UdpServer.cpp
+++ src/server/UdpServer.cpp
@@ -1,26 +1,22 @@
+#include "Socket.h" // Чтобы создать объект Socket()
 #include "UdpServer.h"
 #include "logger.h"
 
 #include <algorithm> // all_of()
 #include <cstring>
 
-
-UdpServer::UdpServer(SessionManager& sessionManager,
-                     CdrWriter& cdrWriter,
+UdpServer::UdpServer(ISessionManager& sessionManager,
+                     ICdrWriter& cdrWriter,
+                     std::unique_ptr<ISocket> socket,
                      pgw::types::ConstIp ip,
                      pgw::types::Port port)
     : m_sessionManager{sessionManager},
-      m_cdrWriter{cdrWriter}{
-    try{
-        m_socket = Socket();
-        m_socket.bind(ip,port);
-    }
-    catch (const std::exception& e){
-        Logger::error("UDP server initialised failed " + std::string(ip) + ":" + std::to_string(port) + " - " + std::string(e.what()));
-        // Пробрасываем с дополнительным контекстом
-        std::throw_with_nested(std::runtime_error("UDP server initialization failed"));
-    }
-    Logger::info("UDP server initialised");
+      m_cdrWriter{cdrWriter},
+      m_socket{std::move(socket)},
+      m_ip{ip},
+      m_port{port},
+      m_running{false}{
+    Logger::info("UDP server initialized");
 }
 
 UdpServer::~UdpServer(){
@@ -32,7 +28,15 @@ void UdpServer::start(){
         Logger::warn("UDP server already running");
         return;
     }
-    m_running = true;
+    try{
+        m_socket->bind(m_ip,m_port);
+        m_running = true;
+    }
+    catch (const std::exception& e){
+        Logger::error("UDP server start failed " + std::string(m_ip) + ":" + std::to_string(m_port) + " - " + std::string(e.what()));
+        // Пробрасываем с дополнительным контекстом
+        std::throw_with_nested(std::runtime_error("UDP server start failed"));
+    }
     Logger::info("UDP server sucseccdully start");
     // run();
 }
@@ -47,28 +51,28 @@ void UdpServer::stop(){
 void UdpServer::run(){
     if(!m_running) return;
     try{
-        auto packet = m_socket.recieve();
+        auto packet = m_socket->receive();
         if(!packet.data.empty()){
             std::string answer {"rejected"};
             std::string imsi = packet.data;
             // Валидация
             if (validateImsi(imsi)) {
-                m_socket.send(answer, packet.senderAddr);
+                m_socket->send(answer, packet.senderAddr);
                 return;
             }
 
             switch (m_sessionManager.createSession(imsi)) {
-                case SessionManager::CreateResult::CREATED:
+                case ISessionManager::CreateResult::CREATED:
                     answer = "created";
                     m_cdrWriter.writeAction(imsi, answer);
                     break;
-                case SessionManager::CreateResult::REJECTED_BLACKLIST:
+                case ISessionManager::CreateResult::REJECTED_BLACKLIST:
                     m_cdrWriter.writeAction(imsi, answer);
-                case SessionManager::CreateResult::ALREADY_EXISTS:
-                case SessionManager::CreateResult::ERROR:
+                case ISessionManager::CreateResult::ALREADY_EXISTS:
+                case ISessionManager::CreateResult::ERROR:
                 default: break;
             }
-            m_socket.send(answer, packet.senderAddr);
+            m_socket->send(answer, packet.senderAddr);
         }
     }
     catch(const std::exception& e){
@@ -85,5 +89,5 @@ bool UdpServer::validateImsi(const std::string& imsi){
         Logger::warn("UDP server receive imsi whith invalid symbols: " + std::string(imsi));
         return false;
     }
-    return ;
+    return true;
 }
