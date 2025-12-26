b86944827cd174ade5317d54d6b04c9e58b72cd9
Artem <powerranger1912@gmail.com>
2025-12-26 18:02:28 +0300

    fix(socket): исправил возвращаемый тип метода send() на void

--- tests/mocks/MockSocket.h
+++ tests/mocks/MockSocket.h
@@ -10,7 +10,7 @@ public:
     // MockSocket() = default;
     // virtual ~MockSocket() = default;
     MOCK_METHOD(void, bind, (pgw::types::ConstIp ip, pgw::types::Port port), (override));
-    MOCK_METHOD(bool, send, (std::string_view data, const sockaddr_in& address), (override));
+    MOCK_METHOD(void, send, (std::string_view data, const sockaddr_in& address), (override));
     MOCK_METHOD(Packet, receive, (), (override));
     MOCK_METHOD(void, close, (), (override));
     MOCK_METHOD(int, getFd, (), (const override));
