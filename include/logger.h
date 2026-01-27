b83f7dd14c19c348887cbbc077d0d55fb8427b62
Artem <powerranger1912@gmail.com>
2026-01-28 01:41:17 +0300

    feat(include): include директорий с общими для клиента и сервера файлами; в файл Сокета был добавлен send(...) принимающий ip и порт; к аллиасам общих для проекта типов был добавлен суффикс '_t'

---
+++ include/logger.h
@@ -0,0 +1,39 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <spdlog/spdlog.h>
+
+namespace logger {
+
+    using level = spdlog::level::level_enum;
+
+    // Инициализация работы оггера
+    void init(
+        std::string_view logFile,
+        std::string_view logLevel,
+        size_t maxFileSize = 5 * 1024 * 1024,
+        size_t maxFiles = 3
+    );
+
+    // Остановка логгера и сброс всех сообщений
+    void shutdown();
+
+    // Инициализирован ли логгер?
+    bool isInit();
+
+    // Превращение std::string_view в spdlog::level::level_enum
+    level parse_level(std::string_view level);
+
+    // Установка уровня логирования
+    void set_level(spdlog::level::level_enum level);
+};
+
+// Макросы для логирования
+#define LOG_TRACE(...)    spdlog::trace(__VA_ARGS__)
+#define LOG_DEBUG(...)    spdlog::debug(__VA_ARGS__)
+#define LOG_INFO(...)     spdlog::info(__VA_ARGS__)
+#define LOG_WARN(...)     spdlog::warn(__VA_ARGS__)
+#define LOG_ERROR(...)    spdlog::error(__VA_ARGS__)
+#define LOG_CRITICAL(...) spdlog::critical(__VA_ARGS__)
+
+#endif // LOGGER_H
