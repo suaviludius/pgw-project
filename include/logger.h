be0bdc018d2c870603f0c6d24ac98eb9049956a5
Artem <powerranger1912@gmail.com>
2026-01-28 22:16:31 +0300

    feat(logger): логгер помещен в namespace pgw

--- include/logger.h
+++ include/logger.h
@@ -3,30 +3,33 @@
 
 #include <spdlog/spdlog.h>
 
+namespace pgw {
 namespace logger {
 
-    using level = spdlog::level::level_enum;
+using level = spdlog::level::level_enum;
 
-    // Инициализация работы оггера
-    void init(
-        std::string_view logFile,
-        std::string_view logLevel,
-        size_t maxFileSize = 5 * 1024 * 1024,
-        size_t maxFiles = 3
-    );
+// Инициализация работы оггера
+void init(
+    std::string_view logFile,
+    std::string_view logLevel,
+    size_t maxFileSize = 5 * 1024 * 1024,
+    size_t maxFiles = 3
+);
 
-    // Остановка логгера и сброс всех сообщений
-    void shutdown();
+// Остановка логгера и сброс всех сообщений
+void shutdown();
 
-    // Инициализирован ли логгер?
-    bool isInit();
+// Инициализирован ли логгер?
+bool isInit();
 
-    // Превращение std::string_view в spdlog::level::level_enum
-    level parse_level(std::string_view level);
+// Превращение std::string_view в spdlog::level::level_enum
+level parse_level(std::string_view level);
 
-    // Установка уровня логирования
-    void set_level(spdlog::level::level_enum level);
-};
+// Установка уровня логирования
+void set_level(spdlog::level::level_enum level);
+
+} // namespace logger
+} // namespace pgw
 
 // Макросы для логирования
 #define LOG_TRACE(...)    spdlog::trace(__VA_ARGS__)
