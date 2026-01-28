e41874738df2807bf5f453020576e4e6b8b2d743
Artem <powerranger1912@gmail.com>
2026-01-28 22:32:12 +0300

    feat(i-session-manager): добавлена функция добавления в черный список addToBlacklist()

--- pgw_server/interfaces/ISessionManager.h
+++ pgw_server/interfaces/ISessionManager.h
@@ -16,6 +16,7 @@ public:
 
     virtual CreateResult createSession(pgw::types::constImsi_t imsi) = 0;
     virtual bool hasSession(pgw::types::constImsi_t imsi) const = 0;
+    virtual bool addToBlacklist(pgw::types::constImsi_t imsi) = 0;
     virtual size_t countActiveSession() const = 0;
 };
 
