1797be783a9965ade396e4595c2a106c421464d6
suaviludius <powerranger1912@gmail.com.com>
2026-03-11 16:48:01 +0300

    perf(udp-server): создание сокета через SocketFactory::create(), тип соккета стал IUdpSocket; удалена проверка m_running в handler

--- pgw_server/UdpServer.h
+++ pgw_server/UdpServer.h
@@ -3,31 +3,20 @@
 
 #include "ISessionManager.h"
 #include "ICdrWriter.h"
-#include "ISocket.h"
+#include "IUdpSocket.h"
 
-#include <memory> // unique_ptr
-
-// UDP сервер для обработки пакетов от абонентов
-// Принимает UDP датаграммы, валидирует IMSI и передает управление SessionManager
-// Предоставляет интерфейс для интеграции с poll/epoll
+#include <memory>
 
 namespace pgw {
 
 class UdpServer{
 private:
-    // Максимальный размер принимаемого UDP пакета
     static constexpr uint32_t MAX_DATAGRAM_SIZE = 1500;
 
-    // Ссылка на менеджер сессий (ассоциация)
     ISessionManager& m_sessionManager;
+    std::unique_ptr<IUdpSocket> m_socket;
 
-    // Умный указатель на сокет
-    std::unique_ptr<ISocket> m_socket;
-
-    // IP-адрес сервера
     pgw::types::ip_t m_ip;
-
-    // Порт сервера
     pgw::types::port_t m_port;
 
     bool m_running;
@@ -37,27 +26,17 @@ public:
         ISessionManager& sessionManager,
         pgw::types::constIp_t ip,
         pgw::types::port_t port,
-        std::unique_ptr<ISocket> socket = nullptr
+        std::unique_ptr<IUdpSocket> socket = nullptr
     );
     ~UdpServer();
 
-    // Запускает UDP сервер (биндит сокет к указанному адресу и порту)
     void start();
-
-    // Останавливает UDP сервер
     void stop();
-
-    // Обработчик входящих пакетов. Вызывается из основного цикла при наличии данных
-    // Читает доступные датаграммы, валидирует IMSI и создает сессии
     void handler();
 
-    //  Проверяет, запущен ли сервер
-    bool isRunning() const {return m_running;}
+    bool isRunning() const { return m_running; }
+    int getFd() const { return m_socket->getFd(); }
 
-    // Возвращает файловый дескриптор сокета для использования с poll/epoll
-    int getFd() const {return m_socket->getFd();};
-
-    // Проверяет длину, допустимые символы imsi
     bool validateImsi(const std::string& imsi);
 };
 
