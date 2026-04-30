f47f748934ba10f0bed1224f3b6818b7b1b1ceb3
Artem <powerranger1912@gmail.com>
2026-04-30 20:13:12 +0300

    test(session-manager): объявлены mock методы getActiveSessions(), getStatistics() и terminateSession()

--- tests/mocks/MockSessionManager.h
+++ tests/mocks/MockSessionManager.h
@@ -7,12 +7,13 @@
 
 class MockSessionManager : public ISessionManager {
 public:
-    // MockSessionManager() = default; // Конструктор по умолчанию - у интерфейса нет параметров
-    // virtual ~MockSessionManager() = default;
     MOCK_METHOD(CreateResult, createSession, (const pgw::types::imsi_t& imsi), (override));
     MOCK_METHOD(bool, hasSession, (const pgw::types::imsi_t& imsi), (const, override));
     MOCK_METHOD(bool, addToBlacklist, (const pgw::types::imsi_t& imsi), (override));
     MOCK_METHOD(size_t, countActiveSession, (), (const, override));
+    MOCK_METHOD(const sessions, getActiveSessions, (), (const, override));
+    MOCK_METHOD(Statistics, getStatistics, (), (const, override));
+    MOCK_METHOD(bool, terminateSession, (const pgw::types::imsi_t& imsi), (override));
 };
 
 #endif // MOCK_SESSION_MANAGER_H
