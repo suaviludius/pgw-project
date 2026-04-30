2e5411b7d81892eebc7f41bc12bdc2d49562f465
Artem <powerranger1912@gmail.com>
2026-04-30 19:32:24 +0300

    test(logger): добавлена проверка макроса TEST_LOG для  включения  логов при тестировании

--- tests/mocks/logger.h
+++ tests/mocks/logger.h
@@ -3,12 +3,22 @@
 
 #include <iostream>
 
-// Простые заглушки, которые выводят в std::cout и игнорируют агрументы фрматирования
-#define LOG_INFO(msg, ...)    std::cout << "[INFO]  " << msg << std::endl
-#define LOG_ERROR(msg, ...)   std::cout << "[ERROR] " << msg << std::endl
-#define LOG_DEBUG(msg, ...)   std::cout << "[DEBUG] " << msg << std::endl
-#define LOG_WARN(msg, ...)    std::cout << "[WARN]  " << msg << std::endl
-#define LOG_TRACE(msg, ...)   std::cout << "[TRACE] " << msg << std::endl
-#define LOG_CRITICAL(msg, ...) std::cout << "[CRIT]  " << msg << std::endl
+#ifndef TEST_LOG
+    // Полное игнорирование
+    #define LOG_INFO(msg, ...)      ((void)0)
+    #define LOG_DEBUG(msg, ...)     ((void)0)
+    #define LOG_ERROR(msg, ...)     ((void)0)
+    #define LOG_WARN(msg, ...)      ((void)0)
+    #define LOG_TRACE(msg, ...)     ((void)0)
+    #define LOG_CRITICAL(msg, ...)  std::cout << "[CRIT]  " << msg << std::endl
+#else
+    // Простые заглушки, которые выводят в std::cout и игнорируют агрументы фрматирования
+    #define LOG_INFO(msg, ...)      std::cout << "[INFO]  " << msg << std::endl
+    #define LOG_ERROR(msg, ...)     std::cout << "[ERROR] " << msg << std::endl
+    #define LOG_DEBUG(msg, ...)     std::cout << "[DEBUG] " << msg << std::endl
+    #define LOG_WARN(msg, ...)      std::cout << "[WARN]  " << msg << std::endl
+    #define LOG_TRACE(msg, ...)     std::cout << "[TRACE] " << msg << std::endl
+    #define LOG_CRITICAL(msg, ...)  std::cout << "[CRIT]  " << msg << std::endl
+#endif
 
 #endif // LOGGER_H
