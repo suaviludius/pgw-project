fc90f78cf20cd68fb189283d697efe9d67e8ac29
Artem <powerranger1912@gmail.com>
2026-05-22 21:25:51 +0300

    refactor(logger): перенесен макрос LOG_OFF из tests\mock\logger, он создает заглушки для игнорирования команд логирования; удален  tests\mock\logger; добавлены в СMakeLists - target_compile_definitions() для выключения логирования в тестах

--- include/pgw/common/logger.h
+++ include/pgw/common/logger.h
@@ -1,6 +1,18 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#ifdef LOG_OFF
+
+    // Полное игнорирование
+    #define LOG_INFO(msg, ...)      ((void)0)
+    #define LOG_DEBUG(msg, ...)     ((void)0)
+    #define LOG_ERROR(msg, ...)     ((void)0)
+    #define LOG_WARN(msg, ...)      ((void)0)
+    #define LOG_TRACE(msg, ...)     ((void)0)
+    #define LOG_CRITICAL(msg, ...)  std::cout << "[CRIT]  " << msg << std::endl
+
+#else
+
 #include <spdlog/spdlog.h>
 
 namespace pgw {
@@ -40,5 +52,7 @@ void set_level(spdlog::level::level_enum level);
 #define LOG_WARN(...)     spdlog::warn(__VA_ARGS__)
 #define LOG_ERROR(...)    spdlog::error(__VA_ARGS__)
 #define LOG_CRITICAL(...) spdlog::critical(__VA_ARGS__)
+
+#endif
 
 #endif // LOGGER_H
